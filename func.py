dfe8d17d319f69cefd6a86c7dfa53c4d5503b8ce
Denis Prusakov <Denikpr@gmail.com>
2023-08-22 20:29:52 +0300

    def sort_date,сортировка по дате

--- func.py
+++ func.py
@@ -16,7 +16,7 @@ def load_operation():
 
 def is_executed(operations):
     '''
-    Делаем выборку из словаря по EXECUTED
+    Делаем выборку из словаря по EXECUTED,
     Возвращаем словать.
     '''
     operation_executed = []
@@ -24,3 +24,15 @@ def is_executed(operations):
         if 'state' in operation and operation['state'] =='EXECUTED':
             operation_executed.append(operation)
     return operation_executed
+
+def sort_date(operation_executed):
+    '''
+    Делаем сортировку по дате,
+    последние на первом месте(reverse).
+    '''
+    datelist = []
+    for oper in operation_executed:
+        if 'date' in oper:
+            datelist.append(oper['date'])
+    datelist.sort(reverse = True)
+    return datelist
